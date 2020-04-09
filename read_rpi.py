8ada9b53f10c7919a3a3fc656e08fe424a7f6cef
smiljanic997 <slaven.teslic@gmail.com>
2020-04-09 18:16:21 +0200

    close to final version

--- read_rpi.py
+++ read_rpi.py
@@ -4,33 +4,46 @@ import logging
 from socket import gaierror
 import pickle
 from os import path
+import re
 import vis
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(message)s')
 
+
 def create_parser():
-    parser = argparse.ArgumentParser(prog='IKM_OscRemote', description='IKM oscilloscope remote access.', argument_default=argparse.SUPPRESS)
-    parser.add_argument('hostname', help='Hostname ili IP adresa. Unos u formatu "proxyBLA.BLA" ili "hostname=proxyBLA.BLA"')
-    parser.add_argument('port', help='Port. Unos u formatu "12345" ili "port=12345"')
-    parser.add_argument('--username', default=None, help='Korisnicko ime. Unos u formatu "--username name". Default: pi')
-    parser.add_argument('--password', default=None, help='Lozinka. Unos u formatu "--password pass". Default: raspberry')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+    parser = argparse.ArgumentParser(
+        prog='IKM_OscRemote', description='IKM oscilloscope remote access.', argument_default=argparse.SUPPRESS)
+    parser.add_argument(
+        'hostname', help='Hostname ili IP adresa. Unos u formatu "proxyBLA.BLA" ili "hostname=proxyBLA.BLA"')
+    parser.add_argument(
+        'port', help='Port. Unos u formatu "12345" ili "port=12345"')
+    parser.add_argument('--username', default=None,
+                        help='Korisnicko ime. Unos u formatu "--username name". Default: pi')
+    parser.add_argument('--password', default=None,
+                        help='Lozinka. Unos u formatu "--password pass". Default: raspberry')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s 1.0')
     return parser
+
 
 def get_connection_params(args):
     if args.username is None and args.password is None:
-        logging.warning('Using default username("pi") and password("raspberry").\n')
+        logging.warning(
+            'Using default username("pi") and password("raspberry").\n')
     elif args.username is None:
         logging.warning('Using default username("pi").\n')
     elif args.password is None:
         logging.warning('Using default password("raspberry").\n')
-    
+
     connection_params = {}
     connection_params['username'] = args.username if args.username is not None else 'pi'
     connection_params['password'] = args.password if args.password is not None else 'raspberry'
-    connection_params['hostname'] = args.hostname.split('=')[1] if '=' in args.hostname else args.hostname
-    connection_params['port'] = args.port.split('=')[1] if '=' in args.port else args.port
+    connection_params['hostname'] = args.hostname.split(
+        '=')[1] if '=' in args.hostname else args.hostname
+    connection_params['port'] = args.port.split(
+        '=')[1] if '=' in args.port else args.port
     return connection_params
+
 
 def receive_file(ssh_client, remote_path, local_path):
     """
@@ -39,7 +52,8 @@ def receive_file(ssh_client, remote_path, local_path):
     ftp_client = ssh_client.open_sftp()
     ftp_client.get(remote_path, local_path)
     ftp_client.close()
-    
+
+
 def open_connection(connection_params):
     """
     Open SSH connection.
@@ -56,15 +70,59 @@ def open_connection(connection_params):
                        password=connection_params['password'])
     return ssh_client
 
+
+def dump_in_file(osc_params):
+    with open('.previous_osc_params.pickle', 'wb') as f:
+        pickle.dump(osc_params, f, protocol=pickle.DEFAULT_PROTOCOL)
+
+
+def get_input(input_type):
+    # this regex matches decimals(both floats and ints)
+    exp = '^[0-9]\d*(\.\d+)?$'
+    user_input = ''
+    if input_type == 'channel':
+        while True:
+            user_input = input('Broj kanala(1/2)? ')
+            if user_input == '1' or user_input == '2':
+                break
+    elif input_type == 's_div':
+        while True:
+            user_input = input('s/div[sec]? ')
+            if re.fullmatch(exp, user_input):
+                if float(user_input) < 50.0:
+                    break
+    elif input_type == 'v_div':
+        while True:
+            user_input = input('V/div[V]? ')
+            if re.fullmatch(exp, user_input):
+                if float(user_input) < 50.0:
+                    break
+    elif input_type == 'trig_type':
+        while True:
+            user_input = input('Trigger type[EDGE, PULSE]? ')
+            if user_input.upper() == 'EDGE' or user_input.upper() == 'PULSE':
+                break
+    elif input_type == 'trig_slope':
+        while True:
+            user_input = input('Trigger slope[POS, NEG]? ')
+            if user_input.upper() == 'POS' or user_input.upper() == 'NEG':
+                break
+    elif input_type == 'trig_level':
+        while True:
+            user_input = input('Trigger level[V]? ')
+            if re.fullmatch(exp, user_input):
+                break
+    return user_input
+
+
 def get_oscilloscope_params():
     if not path.exists('.previous_osc_params.pickle'):
-        # create file with default parameters
-        # this is only done the first time the script runs
-        # using pickle to save dictionary
-        default_osc_params = {'p1' : 'parameter 1', 'p2' : 'parameter 2'}
+        default_osc_params = {'channel': '1', 's_div': '0.5', 'v_div': '2',
+                              'trig_type': 'EDGE', 'trig_slope': 'NEG', 'trig_level': '2'}
         with open('.previous_osc_params.pickle', 'wb') as f:
-            pickle.dump(default_osc_params, f, protocol=pickle.DEFAULT_PROTOCOL)
-    
+            pickle.dump(default_osc_params, f,
+                        protocol=pickle.DEFAULT_PROTOCOL)
+
     osc_params = {}
     use_prev_params = None
     while True:
@@ -74,22 +132,34 @@ def get_oscilloscope_params():
                 osc_params = pickle.load(f)
             break
         elif use_prev_params == 'n':
-            # load from std input
+            osc_params['channel'] = get_input('channel')
+            osc_params['s_div'] = get_input('s_div')
+            osc_params['v_div'] = get_input('v_div')
+            osc_params['trig_type'] = get_input('trig_type')
+            osc_params['trig_slope'] = get_input('trig_slope')
+            osc_params['trig_level'] = get_input('trig_level')
+            dump_in_file(osc_params)
             break
-    
     return osc_params
+
 
 if __name__ == "__main__":
     parser = create_parser()
     connection_params = get_connection_params(parser.parse_args())
-    
+
     try:
-        ssh_client = open_connection(connection_params) 
-        # stdin, stdout, stderr = ssh_client.exec_command('python3 /home/pi/slaven/testing_py/read_values_v2.py 1.5')
-        # print(stdout.readlines())
-        # receive_file(ssh_client, '/home/pi/slaven/testing_py/.to_send_v2.pickle', vis.LOCAL_PATH) # TODO > try tempfile here
+        osc_params = get_oscilloscope_params()
+        command = 'sudo python3 /home/pi/slaven/testing_py/scope_settings1.py channel={} trigger_mode={} sweep=SING trig_level={} volt_scale={} time_scale={} --edge_sens=0.5 --edge_slope={} --chann1_offset=0 --chann2_offset=-5'.format(
+            osc_params['channel'], osc_params['trig_type'], osc_params['trig_level'], osc_params['v_div'], osc_params['s_div'], osc_params['trig_slope'])
+
+        print(command)
+        ssh_client = open_connection(connection_params)
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        print(stdout.readlines())
+        receive_file(ssh_client, '/home/pi/slaven/testing_py/.to_send.pickle', vis.LOCAL_PATH)  # TODO > try tempfile here
     except paramiko.AuthenticationException:
-        logging.error('Connection failed. Check credentials(username and password).')
+        logging.error(
+            'Connection failed. Check credentials(username and password).')
     except paramiko.BadHostKeyException:
         logging.error('Bad host key exception occurred.\n')
     except gaierror as err:
