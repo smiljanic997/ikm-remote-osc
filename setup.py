eadee18147048a5e2bc9974ec895da987b8880cd
smiljanic997 <slaven.teslic@gmail.com>
2020-04-12 13:57:33 +0200

    deleted setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from pip._internal.req import parse_requirements
-from setuptools import setup
-
-def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
-
-setup(name="read_rpi", install_requires=load_requirements("requirements.txt"))
